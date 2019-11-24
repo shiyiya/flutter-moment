465a68aa1acc44ec36c7250dce3392e62382013d
CY <i@runtua.cn>
2019-11-24 17:27:47 +0800

    导入导出

--- lib/pages/setting.dart
+++ lib/pages/setting.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:moment/components/card_with_title.dart';
+import 'package:moment/service/event_bus.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:moment/provides/theme.dart';
@@ -27,28 +29,53 @@ class _SettingState extends State<Setting> {
       ),
       body: ListView(
         children: <Widget>[
-          ListTile(
-            title: Text('选择主题'),
-            leading: Icon(Icons.format_color_fill),
-            trailing:
-                Icon(Icons.chevron_right, color: Theme.of(context).accentColor),
-            onTap: _buildThemeSwitchDialog,
+          CardWithTitle(
+            title: '外观',
+            children: <Widget>[
+              ListTile(
+                title: Text('选择主题'),
+                leading: Icon(Icons.format_color_fill),
+                trailing: Icon(Icons.chevron_right,
+                    color: Theme.of(context).accentColor),
+                onTap: _buildThemeSwitchDialog,
+              ),
+            ],
           ),
-          Divider(height: 0),
-          ListTile(
-            title: Text('关于'),
-            leading: Icon(Icons.info),
-            trailing:
-                Icon(Icons.chevron_right, color: Theme.of(context).accentColor),
-            onTap: () => Navigator.of(context).pushNamed('/about'),
+          CardWithTitle(
+            title: '备份 & 恢复',
+            children: <Widget>[
+              ListTile(
+                title: Text('覆盖导入'),
+                leading: Icon(Icons.archive),
+                trailing: Icon(
+                  Icons.chevron_right,
+                  color: Theme.of(context).accentColor,
+                ),
+                onTap: _import,
+              ),
+              ListTile(
+                title: Text('导出 ZIP'),
+                leading: Icon(Icons.unarchive),
+                trailing: Icon(
+                  Icons.chevron_right,
+                  color: Theme.of(context).accentColor,
+                ),
+                onTap: _export,
+              ),
+            ],
           ),
-          Divider(height: 0),
-          // ListTile(
-          //   title: Text('导入 / 导出'),
-          //   leading: Icon(Icons.import_export),
-          //   onTap: _buildImpExpDialog,
-          // ),
-          // Divider(height: 0),
+          CardWithTitle(
+            title: '其他',
+            children: <Widget>[
+              ListTile(
+                title: Text('关于'),
+                leading: Icon(Icons.info),
+                trailing: Icon(Icons.chevron_right,
+                    color: Theme.of(context).accentColor),
+                onTap: () => Navigator.of(context).pushNamed('/about'),
+              ),
+            ],
+          ),
         ],
       ),
     );
@@ -63,13 +90,19 @@ class _SettingState extends State<Setting> {
                 RadioListTile(
                   groupValue: theme.value,
                   value: 0,
-                  title: Text('light'),
+                  title: Text('Teal'),
                   onChanged: (i) => setTheme(theme, i),
                 ),
                 RadioListTile(
                   groupValue: theme.value,
                   value: 1,
-                  title: Text('dark'),
+                  title: Text('Light'),
+                  onChanged: (i) => setTheme(theme, i),
+                ),
+                RadioListTile(
+                  groupValue: theme.value,
+                  value: 2,
+                  title: Text('Dark'),
                   onChanged: (i) => setTheme(theme, i),
                 )
               ]),
@@ -81,108 +114,90 @@ class _SettingState extends State<Setting> {
     Navigator.of(context).pop();
   }
 
-  _buildImpExpDialog() {
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            content: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                IconButton(
-                    onPressed: _import,
-                    tooltip: '导入',
-                    icon: Icon(Icons.vertical_align_bottom)),
-                IconButton(
-                    onPressed: _export,
-                    tooltip: '导出',
-                    icon: Icon(Icons.vertical_align_top))
-              ],
-            ),
-          );
-        });
-  }
-
   _import() async {
 //    Map<PermissionGroup, PermissionStatus> permissions =
     await PermissionHandler().requestPermissions([PermissionGroup.storage]);
 
     String filePath = await FilePicker.getFilePath(type: FileType.ANY);
+    if (!filePath.endsWith('zip') || !filePath.contains('moment-backup')) {
+      _showDialog(context, '错误', '格式错误，请选择正确的 ZIP 备份包');
+      return;
+    }
+
     List<int> bytes = File(filePath).readAsBytesSync();
     Archive archive = ZipDecoder().decodeBytes(bytes);
     String dbDir = await getDatabasesPath();
 
     String picPath = (await getExternalStorageDirectory()).path + '/Pictures';
 
-    for (ArchiveFile file in archive) {
-      String filename = file.name;
-      String type = filename.substring(filename.length - 2, filename.length);
+    try {
+      for (ArchiveFile file in archive) {
+        String filename = file.name;
+        String type = filename.substring(filename.length - 2, filename.length);
 
-      List<int> data = file.content;
-      if (file.isFile) {
-        if (type == 'db') {
-          File(dbDir + 'moment.db')
-            ..createSync(recursive: true)
-            ..writeAsBytesSync(data);
+        List<int> data = file.content;
+        if (file.isFile) {
+          if (type == 'db') {
+            File(dbDir + '/moment.db')
+              ..createSync(recursive: true)
+              ..writeAsBytesSync(data);
+          } else {
+            File(picPath + filename)
+              ..createSync(recursive: true)
+              ..writeAsBytesSync(data);
+          }
         } else {
-          File(picPath + filename)
-            ..createSync(recursive: true)
-            ..writeAsBytesSync(data);
-        }
-      } else {
 //        Directory('out/' + filename)..create(recursive: true);
+        }
       }
-    }
+    } catch (e) {}
+    _showDialog(context, '成功', '导入成功，一切似乎都很顺利~');
+    eventBus.fire(HomeRefreshEvent(true));
   }
 
   _export() async {
 //    Map<PermissionGroup, PermissionStatus> permissions =
     await PermissionHandler().requestPermissions([PermissionGroup.storage]);
 
+    try {
+      String timeMS = DateTime.now().millisecondsSinceEpoch.toString();
+
+      String picPath = (await getExternalStorageDirectory()).path + '/Pictures';
+
+      File(await DBHelper().getDatabasePath()).copySync(
+          '$picPath/moment-sqlite-${Date.getDateFormatYMD()}-$timeMS.db');
+
+      String outFileName =
+          'moment-backup-${Date.getDateFormatYMD()}-$timeMS.zip';
+
+      await MPath.encodeDirFile2Download(picPath, outFileName);
+
+      // 删除复制的 DB
+      File('$picPath/moment-sqlite-${Date.getDateFormatYMD()}-$timeMS.db')
+          .delete();
+
+      _showDialog(context, '成功', '导出成功，导出路径：/Download/$outFileName', children: [
+        FlatButton(
+          child: Text('确定'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        )
+      ]);
+    } catch (_) {
+      _showDialog(context, '失败', '备份失败了呢');
+    }
+  }
+
+  _showDialog(BuildContext _, String title, String content,
+      {List<Widget> children}) {
     showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return SimpleDialog(
-            children: <Widget>[Center(child: CircularProgressIndicator())],
-          );
-        });
-
-    await Future.delayed(Duration(microseconds: 500));
-
-    String timeMS = DateTime.now().millisecondsSinceEpoch.toString();
-
-    String picPath = (await getExternalStorageDirectory()).path + '/Pictures';
-
-    File(await DBHelper().getDatabasePath()).copySync(
-        '$picPath/moment-sqlite-${Date.getDateFormatYMD()}-$timeMS.db');
-
-    String outFileName = 'moment-backup-${Date.getDateFormatYMD()}-$timeMS.zip';
-
-    await MPath.encodeDirFile2Download(picPath, outFileName);
-
-    Navigator.pop(context);
-
-    // 删除复制的 DB
-    File('$picPath/moment-sqlite-${Date.getDateFormatYMD()}-$timeMS.db')
-        .delete();
-
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            content: Text('导出成功，/Download/$outFileName'),
-            actions: <Widget>[
-              FlatButton(
-                child: Text(
-                  '确定',
-                  style: TextStyle(color: Theme.of(context).primaryColor),
-                ),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        });
+      context: _,
+      builder: (_) => AlertDialog(
+        title: Text(title),
+        content: Text(content),
+        actions: children,
+      ),
+    );
   }
 }
